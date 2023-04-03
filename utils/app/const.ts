0f69a1f10e7fb47e06fb46a53b1f0cb364dcaf73
Igli Manaj <manajigli@gmail.com>
2023-04-03 11:57:35 +0200

    default system prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
-  process.env.DEFAULT_SYSTEM_PROMPT || "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  process.env.DEFAULT_SYSTEM_PROMPT || "You are amberAI model, a large language model. Follow the user's instructions carefully. Respond using markdown. Answer in the language of the prompt. Here you can find some context about amberSearch. amberSearch is the most advanced enterprise search engine, that uses AI for delivering the most accurate search result to the user. It was developed by the german startup with the same name.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
